3fb4a577900eb83bc9289d7325238a11e54d41c5
ccccccjh <72149771+Coiggahou2002@users.noreply.github.com>
2023-08-23 21:39:09 +0800

    chore: 修改 site URL 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://coiggahou2002.github.io/blog',
+	site: 'https://coiggahou2002.github.io/rory-blog',
 	integrations: [mdx(), sitemap()],
 });
